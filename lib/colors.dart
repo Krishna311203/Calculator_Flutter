2b18bed91dad084c6bb7df7a76dfe405bc53f6ed
Krishna311203 <krishnashrivastava84@gmail.com>
2023-01-28 00:35:40 +0530

    completed

---
+++ lib/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const operatorColor = Color.fromARGB(55, 33, 173, 89);
+const buttonColor = Color.fromARGB(255, 80, 213, 19);
+const greenColor = Color.fromARGB(255, 0, 255, 8);
